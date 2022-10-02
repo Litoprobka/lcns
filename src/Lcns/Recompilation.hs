c36121a8759d41ef1b4d7a3705acc0f0d22e2ef4
Litoprobka <52280455+Litoprobka@users.noreply.github.com>
2022-10-02 21:35:33 +0300

    Conditional recompilation

--- src/Lcns/Recompilation.hs
+++ src/Lcns/Recompilation.hs
@@ -2,39 +2,63 @@ module Lcns.Recompilation (recompileAndRun) where
 
 import           Relude
 
+import           Control.Exception (handle)
+import           Data.Time         (UTCTime)
 import           System.Directory
+import           System.Exit       (ExitCode (ExitSuccess))
 import           System.FilePath   ((</>))
 import           System.Posix      (executeFile)
-import           System.Process    (createProcess, proc, waitForProcess, cwd)
-import System.Exit (ExitCode(ExitSuccess))
+import           System.Process    (createProcess, cwd, proc, waitForProcess)
+
+data Dirs = Dirs
+    { cache :: FilePath
+    , config :: FilePath
+    }
 
 
 recompileAndRun :: IO ()
 recompileAndRun = do
-    cacheDir <- getXdgDirectory XdgCache "lcns"
-    createDirectoryIfMissing True cacheDir
-    configDir  <- getXdgDirectory XdgConfig "lcns"
+    cache <- getXdgDirectory XdgCache "lcns"
+    createDirectoryIfMissing True cache
+    config  <- getXdgDirectory XdgConfig "lcns"
 
-    -- TODO: check if the program should be recompiled
-    putTextLn "Recompiling lcns..."
-    exitCode <- compile cacheDir configDir
-    guard $ exitCode == ExitSuccess
-    executeFile (cacheDir </> "lcns") False [] Nothing
+    ifM (shouldRecompile Dirs{..})
+        (do
+        putTextLn "Recompiling lcns..."
+        exitCode <- compile Dirs{..}
+        guard $ exitCode == ExitSuccess)
 
-    where
-        compile cache configDir = do
-            (_, _, _, ph) <- createProcess (proc "ghc"
-                [ "--make"
-                , configDir </> "lcns.hs"
-                , "-i"
-                , "-ilib"
-                , "-fforce-recomp"
-                , "-threaded"
-                , "-rtsopts"
-                , "-with-rtsopts=-N"
-                , "-main-is", "main"
-                , "-v0"
-                , "-outputdir", cache </> "build"
-                , "-o", cache </> "lcns"
-                ]){ cwd = Just configDir }
-            waitForProcess ph
+        (putTextLn "Recompilation is not needed")
+        
+    executeFile (cache </> "lcns") False [] Nothing
+
+
+compile :: Dirs -> IO ExitCode
+compile dirs = do
+    (_, _, _, ph) <- createProcess (proc "ghc"
+        [ "--make"
+        , dirs.config </> "lcns.hs"
+        , "-i"
+        , "-ilib"
+        , "-fforce-recomp"
+        , "-threaded"
+        , "-rtsopts"
+        , "-with-rtsopts=-N"
+        , "-main-is", "main"
+        , "-v0"
+        , "-outputdir", dirs.cache </> "build"
+        , "-o", dirs.cache </> "lcns"
+        ]){ cwd = Just dirs.config }
+    waitForProcess ph
+
+shouldRecompile :: Dirs -> IO Bool
+shouldRecompile dirs = do
+    binModTime <- getModTime $ dirs.cache </> "lcns"
+    configModTime <- getModTime $ dirs.config </> "lcns.hs"
+    pure $ fromMaybe True $ (<) <$> binModTime <*> configModTime
+    
+
+getModTime :: FilePath -> IO (Maybe UTCTime)
+getModTime path =
+    handle (\(SomeException _) -> pure Nothing) $ Just <$> getModificationTime path
+
